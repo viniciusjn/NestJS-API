993598a40225f82923831d32618229d0943a0e30
viniciusjn <viniciusaqpsjn@gmail.com>
2025-07-09 12:18:27 -0300

    module | providers add

--- nestjs-api/src/event/event.module.ts
+++ nestjs-api/src/event/event.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventController } from './event.controller';
+import { CreateEventRepository } from './repositories/create-event.repository';
+import { CreateEventService } from './services/create-event.service';
 
 @Module({
-  controllers: [EventController]
+  controllers: [EventController],
+  providers: [CreateEventRepository, CreateEventRepository],
+  exports: [CreateEventRepository, CreateEventService],
 })
 export class EventModule {}
