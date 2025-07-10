3b9871528400a9da0ba0ac9452b74ed3f5083bed
viniciusjn <viniciusaqpsjn@gmail.com>
2025-07-10 10:08:57 -0300

    controller layer

--- nestjs-api/src/event/event.controller.ts
+++ nestjs-api/src/event/event.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateEventService } from './services/create-event.service';
+import { IEventEntity } from './interfaces/IEventEntity';
 
 @Controller('event')
-export class EventController {}
+export class EventController {
+  constructor(
+    private readonly createEventService: CreateEventService
+  ){}
+
+  @Post('create')
+  async create(@Body() event: IEventEntity): Promise<IEventEntity>{
+    return this.createEventService.execute(event);
+  }
+}
